5e0901cb0984bdf3bffed53d489bc15fae2d871e
arvrao <arvindrao.759@gmail.com>
2022-12-28 17:29:09 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render(): React.ReactNode {
+    return (
+      <div className="App">
+        <h1>Hello there</h1>
+      </div>
+    );   
+  }
+}
+
+export default App;
