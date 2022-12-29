950611a0f3c7e355a693e24326c07af3ded78466
arvrao <arvindrao.759@gmail.com>
2022-12-29 20:58:36 +0530

    Added project structure and better routing pattern

--- backend/main.go
+++ backend/main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"fmt"
 
+	"github.com/arvrao/kanban-board-app/router"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	_ "github.com/lib/pq"
 )
-
-func Hello(c *fiber.Ctx) error {
-	return c.Status(200).JSON("Hello there")
-}
 
 func main() {
 	app := fiber.New()
-	app.Get("/hello", Hello)
+	app.Use(logger.New())
+	app.Use(cors.New())
+	router.SetupRoutes(app)
 	fmt.Println("Running on port 3000")
 	app.Listen(":3000")
 }
